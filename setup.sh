9682b6f78e3713c427290622380eaa0cc3d1eca2
tachyontec <92679798+tachyontec@users.noreply.github.com>
2022-01-02 00:44:27 +0200

    Update setup.sh
    Now it takes all user input and replaces variables prices at the main script so it doesn't need any arguments

--- setup.sh
+++ setup.sh
@@ -2,24 +2,60 @@
 
 #NEEDS TO BE RUN WITH SUDO
 
-#This is only for the first time to run the programm
-chmod +x storjMailAlert #Make the script excecutable
+#This is only for the first time to run the programm:
+chmod +x storjMailAlert addCron 2>/dev/null #Make the scripts excecutable
 
-#If you run it without sudo you will have trouble
-#with those commands:
+old_upd_int=20 #We need this variable defined in a classed scope(will be used below)
+#Create a function to update data to use it below
+function update() {
+        #We will save old data to three variables
+        old_receiver=$(cat /bin/storjMailAlert|grep "receiver="|cut -d "=" -f2|xargs)
+        old_upd_int=$(cat /bin/storjMailAlert|grep "upd_int="|cut -d"=" -f2|xargs)
+        old_min=$(cat /bin/storjMailAlert|grep "min="|cut -d"=" -f2|xargs)
 
-#Remove the file in case we are not running setup for 1st time
-rm -f /bin/storjMailAlert #Adding -f to avoid error message
-mv storjMailAlert /bin #Move it to the bin
+        echo "Press enter to use default prices or if you don't want to change the last one"
+        #Gather inputs and add replace values in case we receive something
+        read -p "Receiver email: "  receiver
+        if  [ -z $receiver ]  && [ -z $old_receiver ];then
+                echo "Email not received, don't know where to send the mail"
+                exit
+        elif ! [ -z $receiver ];then
+                #Replace with new receiver
+                sed -i "s/receiver=$old_receiver/receiver=$receiver/" /bin/storjMailAlert
+        fi
+	echo "***if you are updating make sure to delete the old cron task with crontab -e"
+        read -p "How often to run the check?[minutes] " upd_int
+        if ! [ -z $upd_int ];then
+                #Replace with new update interval
+                sed -i "s/upd_int=$old_upd_int/upd_int=$upd_int/" /bin/storjMailAlert
+		old_upd_int=$upd_int #Change this to use it in the end of the script
+        fi
+        read -p "Minimum err% to send mail? " min
+        if ! [ -z $min ];then
+                #Replace with new minimum percentage
+                sed -i "s/min=$old_min/min=$min/" /bin/storjMailAlert
+        fi
+}
 
-#Now you can excecute the command:
-#storjMailAlert $your_email
-#And run the check manually whenever you want
+#Check if we already have
+if ! [ -s storjMailAlert ];then
+        #We haven't found a script in the directory we are running the setup
+        read -p  "No script found do you want to update the existing one in bin?" ans
+        if [ "${ans^^}" = "Y" ]; then
+                update
+        else
+                "You don't have a script in this directory"
+                "Doing nothing..."
+                exit
+        fi
+else
+        #We have a script in this directory
+	echo "Configuring main script..."
+        mv -f storjMailAlert /bin
+        update
+	echo "Command 'storjMailAlert' was created"
+	echo "run it whenever you want to run the check"
+fi
 
 #I am using another script to add the task to cron
-#If you want it to work you have to give the mail
-#in which you want to receive the notifications as $1
-#Comment it out if you want to run it yourself
-#Or you can just add it manually to cron with command:
-#crontab -e
-chmod +x addCron && ./addCron $1
+chmod +x addCron && ./addCron $old_upd_int && echo Task added to cron succesfully
