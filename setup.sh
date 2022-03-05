18c38f3f3ca2a945788bb39430ee5bc10d0550b1
tachyontec <92679798+tachyontec@users.noreply.github.com>
2022-03-05 23:59:42 +0200

    General optimizations

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#NEEDS TO BE RUN WITH SUDO
-
 #This is only for the first time to run the programm:
 chmod +x storjMailAlert addCron 2>/dev/null #Make the scripts excecutable
 
-old_upd_int=20 #We need this variable defined in a classed scope(will be used below)
+old_upd_int=20 #We need this variable defined in file scope(will be used below)
+
 #Create a function to update data to use it below
 function update() {
-        #We will save old data to three variables
+
+        #We will save the old price of each parameter to a variable
         old_receiver=$(cat /bin/storjMailAlert|grep "receiver="|cut -d "=" -f2|xargs)
         old_upd_int=$(cat /bin/storjMailAlert|grep "upd_int="|cut -d"=" -f2|xargs)
         old_min=$(cat /bin/storjMailAlert|grep "min="|cut -d"=" -f2|xargs)
 
         echo "Press enter to use default prices or if you don't want to change the last one"
         #Gather inputs and add replace values in case we receive something
-        read -p "Receiver email: "  receiver
+        read -p "Receiver email(current-> $old_receiver ):"  receiver
         if  [ -z $receiver ]  && [ -z $old_receiver ];then
                 echo "Email not received, don't know where to send the mail"
                 exit
@@ -23,21 +23,21 @@ function update() {
                 #Replace with new receiver
                 sed -i "s/receiver=$old_receiver/receiver=$receiver/" /bin/storjMailAlert
         fi
-	echo "***if you are updating make sure to delete the old cron task with crontab -e"
-        read -p "How often to run the check?[minutes] " upd_int
+        echo "***if you are updating make sure to delete the old cron task with $ crontab -e"
+        read -p "How often to run the check?[minutes] (current-> $old_upd_int ) $ " upd_int
         if ! [ -z $upd_int ];then
                 #Replace with new update interval
                 sed -i "s/upd_int=$old_upd_int/upd_int=$upd_int/" /bin/storjMailAlert
-		old_upd_int=$upd_int #Change this to use it in the end of the script
+                old_upd_int=$upd_int #Change this to use it in the end of the script
         fi
-        read -p "Minimum err% to send mail? " min
+        read -p "Minimum err% to send mail? (current-> $old_min ) " min
         if ! [ -z $min ];then
                 #Replace with new minimum percentage
                 sed -i "s/min=$old_min/min=$min/" /bin/storjMailAlert
         fi
 }
 
-#Check if we already have
+#Check if we already have the script in place
 if ! [ -s storjMailAlert ];then
         #We haven't found a script in the directory we are running the setup
         read -p  "No script found do you want to update the existing one in bin?" ans
@@ -50,12 +50,12 @@ if ! [ -s storjMailAlert ];then
         fi
 else
         #We have a script in this directory
-	echo "Configuring main script..."
-        mv -f storjMailAlert /bin
+        echo "Configuring main script..."
+        mv -f storjMailAlert /bin #Move it to the bin to create the command
         update
-	echo "Command 'storjMailAlert' was created"
-	echo "run it whenever you want to run the check"
+        echo "Command 'storjMailAlert' was created"
+        echo "run it whenever you want to check your logs"
 fi
 
-#I am using another script to add the task to cron
+#Using another script to add the task to cron
 chmod +x addCron && ./addCron $old_upd_int && echo Task added to cron succesfully
