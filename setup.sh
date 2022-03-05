2f4930c8c97c0715fb92c15e202f088e2494cf28
tachyontec <92679798+tachyontec@users.noreply.github.com>
2022-03-06 00:04:11 +0200

    More optimizations

--- setup.sh
+++ setup.sh
@@ -1,61 +1,71 @@
 #!/bin/bash
 
-#This is only for the first time to run the programm:
-chmod +x storjMailAlert addCron 2>/dev/null #Make the scripts excecutable
+mkdir -p  ~/storjAlerts #Make a dir to work on (if not exists)
+cd ~/storjAlerts #Move to this directory
 
-old_upd_int=20 #We need this variable defined in file scope(will be used below)
+#################################################################################
+#As seen in the ./setup we are using sed to assign prices in our varibles,
+#but you can also edit those lines manually to add them
+receiver=your_email
+#This is how often we will check the logs
+upd_int=15
+#Finally we have the minimum percentage of errors required to send an email
+min=5
+#################################################################################
 
-#Create a function to update data to use it below
-function update() {
+#We want to check all logs between two checks
+docker logs storagenode --since "$upd_int"m &> out #Save everything to a txt file
+n_logs=$(cat out|wc -l) #Count the logs that we saved
+grep 'FATAL\|ERROR\|WARN' out  > err #Save errors to err file
+#Counting error logs but we will add 00 in the end so will get the percentage
+err_logs=$(cat err|wc -l)
+#Finally we can count the percentage of errors:
+percent=$(bc <<<"scale=2; $err_logs*100 / $n_logs") #Now that is between 0-100
 
-        #We will save the old price of each parameter to a variable
-        old_receiver=$(cat /bin/storjMailAlert|grep "receiver="|cut -d "=" -f2|xargs)
-        old_upd_int=$(cat /bin/storjMailAlert|grep "upd_int="|cut -d"=" -f2|xargs)
-        old_min=$(cat /bin/storjMailAlert|grep "min="|cut -d"=" -f2|xargs)
+now=$(date +"%T") #get current time
 
-        echo "Press enter to use default prices or if you don't want to change the last one"
-        #Gather inputs and add replace values in case we receive something
-        read -p "Receiver email(current-> $old_receiver ):"  receiver
-        if  [ -z $receiver ]  && [ -z $old_receiver ];then
-                echo "Email not received, don't know where to send the mail"
-                exit
-        elif ! [ -z $receiver ];then
-                #Replace with new receiver
-                sed -i "s/receiver=$old_receiver/receiver=$receiver/" /bin/storjMailAlert
-        fi
-        echo "***if you are updating make sure to delete the old cron task with $ crontab -e"
-        read -p "How often to run the check?[minutes] (current-> $old_upd_int ) $ " upd_int
-        if ! [ -z $upd_int ];then
-                #Replace with new update interval
-                sed -i "s/upd_int=$old_upd_int/upd_int=$upd_int/" /bin/storjMailAlert
-                old_upd_int=$upd_int #Change this to use it in the end of the script
-        fi
-        read -p "Minimum err% to send mail? (current-> $old_min ) " min
-        if ! [ -z $min ];then
-                #Replace with new minimum percentage
-                sed -i "s/min=$old_min/min=$min/" /bin/storjMailAlert
-        fi
-}
+#Check if the percentage is more than the minimum we set:
+if (( $(echo "$percent > $min" |bc -l) ));then
+        #In this case we will create mail:
 
-#Check if we already have the script in place
-if ! [ -s storjMailAlert ];then
-        #We haven't found a script in the directory we are running the setup
-        read -p  "No script found do you want to update the existing one in bin?" ans
-        if [ "${ans^^}" = "Y" ]; then
-                update
-        else
-                "You don't have a script in this directory"
-                "Doing nothing..."
-                exit
-        fi
+        echo "Subject: Storj ERROR Alert" >mail #Add the subject
+        echo " " >>mail #Its really important to let a new line after
+
+        #We will save each error seperately to see if we have repetitions
+        #To make it easier for operator to understand the problem
+        #----------MORE STATS TO BE ADDED----------
+
+        #-f2- is used to remove timestamps to find uniq errors
+        #cut -d { is used to remove specifications about piece id etc...
+        cut -d "Z" -f2- err|cut -d "{" -f1|sort|uniq -c > uniqerr
+        uniq_errors=$(cat uniqerr|wc -l) #Count unique errors
+        echo "---------------------------------------------------" >>mail
+        echo ">Total errors: $err_logs / $n_logs " >>mail
+        echo ">Unique errors: $uniq_errors" >>mail
+        echo ">Total errors percentage: $percent %">>mail
+        echo "---------------------------------------------------">>mail
+        echo "">>mail
+        echo "TIMES_ENCOUNTERED     ERROR">>mail
+        echo "">>mail
+        cat uniqerr >>mail #unique errors and times each appeared
+        echo "<------------------------------------------------------>">>mail
+         echo "">>mail
+        echo "TIMES_ENCOUNTERED     ERROR">>mail
+        echo "">>mail
+        cat uniqerr >>mail #unique errors and times each appeared
+        echo "<------------------------------------------------------>">>mail
+        #After this we add all errors so user can see all the data
+        echo "ALL ERRORS:">>mail
+        echo "">>mail
+        cat err>>mail #Add all errors with timestamps
+        ssmtp $receiver <mail
+        #We will also save logs after every succesful run
+        echo "$now : Error encountered with storjNode" >> storjMailAlertlogs
+        rm out err uniqerr mail #remove files we no longer need
 else
-        #We have a script in this directory
-        echo "Configuring main script..."
-        mv -f storjMailAlert /bin #Move it to the bin to create the command
-        update
-        echo "Command 'storjMailAlert' was created"
-        echo "run it whenever you want to check your logs"
+        #Node is running fine
+        echo "$now : No errors encountered" >> storjMailAlertlogs
 fi
 
-#Using another script to add the task to cron
-chmod +x addCron && ./addCron $old_upd_int && echo Task added to cron succesfully
+#Uncomment this line if you don't want to save script outputs
+#rm -f out err storjMailAlertlogs
